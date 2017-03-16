221b4400526466a875b8d145b6384d152c451b16
Alejandro Esaú Alvarez <alvalex_@hotmail.com>
2017-03-15 23:16:40 -0600

    Se pueden agregar objetos haciendo click
    Se agrega el metodo addObject(object,x,y); para agregar un objeto en el
    la clase escenario.

--- Ejemplos-Greenfoot/Ejemplos/Escenario.java
+++ Ejemplos-Greenfoot/Ejemplos/Escenario.java
@@ -1,29 +1,22 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
+import java.util.ArrayList;
 public class Escenario extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+    Pelota pelota;
     public Escenario()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1,true); 
+        super(600, 400, 1,true);
     }
     @Override
     public void act(){
         if(Greenfoot.mouseClicked(this)){
-            System.out.println("Click en el mundo");
             MouseInfo raton = Greenfoot.getMouseInfo();
-            System.out.println("Coordenadas x: " +raton.getX() + "Coordenadas y: "+raton.getY());
+            int x = raton.getX(); 
+            int y = raton.getY(); 
+            addObject(new Pelota(), x,y);
         }
     }
     
